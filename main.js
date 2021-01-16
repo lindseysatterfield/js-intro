46c4de4a33d5ced75107d5cf1959782ca4a9ce32
Lindsey Satterfield <satterfield.lindsey@gmail.com>
2021-01-16 13:09:22 -0600

    conditional logic

--- main.js
+++ main.js
@@ -1,1 +1,30 @@
-console.log('Hello World!');
+// function thisIsTrue (arg1, arg2) {
+// 	if (arg1 !== 'nice' && arg2 !== 'Nice') {
+// 		console.log(`You are ${arg1}`);
+// 	}
+// 	else {
+// 		console.log(false);
+// 	}
+// }
+
+// function thisIsTrue (arg1, arg2) {
+// 	if (arg1.length < 3) {
+// 		console.log(`You are ${arg1}`);
+// 	}
+// 	else if (arg2.length > 8) {
+// 		console.log(`You are ${arg2}`);
+// 	}
+// 	else {
+// 		console.log('you are NOTHING');
+// 	}
+// }
+
+// thisIsTrue('ni', 'mickeymouse');
+
+const canDrive = (age) => {
+	if (age >= 15) {
+		return true;
+	}
+	return false;
+};
+console.log(canDrive(13));
