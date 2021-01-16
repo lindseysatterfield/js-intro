25ac4dec465b95002e94d665c9d5b4de302f17b3
Lindsey Satterfield <satterfield.lindsey@gmail.com>
2021-01-16 11:07:57 -0600

    added variables, data types, and math to js file

--- main.js
+++ main.js
@@ -1,1 +1,38 @@
-console.log('Hello World!');
+// ***** VARIABLES *****
+let firstName = 'Beyonce';
+let lastName = 'Knowles';
+// console.log(firstName + lastName);
+// console.log(`First Name: ${firstName} Last Name: ${lastName}`);
+
+firstName = 'Dr. T';
+lastName = 'Vasquez';
+// console.log(`First Name: ${firstName} Last Name: ${lastName}`);
+
+const favoriteFood = 'Tacos';
+// console.log(favoriteFood);
+// favoriteFood = 'Pizza';
+// console.log(favoriteFood);
+
+// ***** DATA TYPES *****
+const dog = 'bark';
+// console.log(typeof dog);
+
+const answer = 33;
+const negative = -14;
+// console.log(typeof answer, typeof negative);
+
+const iLoveJavascript = true;
+// console.log(typeof iLoveJavascript);
+
+// ***** MATH *****
+// console.log(Math.pow(2, 2)); // 4
+// console.log(Math.pow(3, 2)); // 9
+// console.log(Math.pow(3, 3)); // 27
+
+// console.log(Math.round(6.5)); // 7
+// console.log(Math.round(6.45)); // 6
+// console.log(Math.floor(2.2)); // 2
+// console.log(Math.ceil(2.2)); // 3
+
+const cat = 'kitty';
+console.log(cat.length); // 5
