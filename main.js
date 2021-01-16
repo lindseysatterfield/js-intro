979c11203d15300ee5cd8b02c4cdbe910f7cdac8
Lindsey Satterfield <satterfield.lindsey@gmail.com>
2021-01-16 11:57:59 -0600

    added functions with arguments and calls

--- main.js
+++ main.js
@@ -1,1 +1,27 @@
-console.log('Hello World!');
+// ***** FUNCTIONS WITHOUT ARGUMENTS *****
+// function sayHello () {
+// 	console.log('Hello');
+// }
+
+// const sayHello = function () {
+// 	console.log('Hello');
+// };
+
+// const sayHello = () => {
+// 	console.log('Hello');
+// };
+
+// sayHello();
+
+// ***** FUNCTIONS WITH ARGUMENTS *****
+// function logsHello (name) {
+// 	console.log(`Hello, ${name}!`);
+// }
+// logsHello('Lindsey');
+
+const addsTwoNumbers = (num1, num2) => {
+	const sum = num1 + num2;
+	console.log(sum);
+	return sum;
+};
+console.log(addsTwoNumbers(5, 7));
